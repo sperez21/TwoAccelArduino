147bc270b7410f418baa986c69c5f0a01e50b7c8
sperez21 <37560564+sperez21@users.noreply.github.com>
2018-03-28 16:12:02 -0700

    Update TwoAccelConnnection.ino

--- TwoAccelConnnection.ino
+++ TwoAccelConnnection.ino
@@ -3,7 +3,7 @@
 #include "MPU6050.h"
 
 const int MPU1_addr = 0x68;
-//const int MPU2_addr = 0x69;
+const int MPU2_addr = 0x69;
 
 int16_t AcXW, AcYW, AcZW;
 int16_t AcXF, AcYF, AcZF;
@@ -13,7 +13,7 @@ void setup() {
   Serial.print("this line");
   Wire.begin();
   Wire.beginTransmission(MPU1_addr);
-//  Wire.beginTransmission(MPU2_addr);
+  Wire.beginTransmission(MPU2_addr);
   Wire.write(0x6B);
   Wire.write(0);
   Wire.endTransmission(true);
@@ -26,7 +26,7 @@ void loop() {
   // put your main code here, to run repeatedly:
   Wire.beginTransmission(MPU1_addr);
   Wire.write(0x3B);
-//  Wire.beginTransmission(MPU2_addr);
+  Wire.beginTransmission(MPU2_addr);
   Wire.endTransmission(false);
 
   Wire.requestFrom(MPU1_addr, 8, true); 
@@ -37,16 +37,16 @@ void loop() {
 
 //  Wire.requestFrom(MPU2_addr, 8, true);
 
-//    AcXF = Wire.read() << 8 | Wire.read();
-//    AcYF = Wire.read() << 8 | Wire.read();
-//    AcZF = Wire.read() << 8 | Wire.read();
+    AcXF = Wire.read() << 8 | Wire.read();
+    AcYF = Wire.read() << 8 | Wire.read();
+    AcZF = Wire.read() << 8 | Wire.read();
 
   Serial.print("AcXW = "); Serial.println(AcXW);
   Serial.print("AcYW = "); Serial.println(AcYW);
   Serial.print("AcZW = "); Serial.println(AcZW);
-//  Serial.print("AcXF = "); Serial.println(AcXF);
-//  Serial.print("AcYF = "); Serial.println(AcYF);
-//  Serial.print("AcZF = "); Serial.println(AcZF);
+  Serial.print("AcXF = "); Serial.println(AcXF);
+  Serial.print("AcYF = "); Serial.println(AcYF);
+  Serial.print("AcZF = "); Serial.println(AcZF);
 
   delay(100);
 }
